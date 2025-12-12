7ca09b90bfe5ac4861a1042853c639df60bcb787
DJEBARRA Rabah Abderrazak <djebarra.ra@gmail.com>
2025-12-12 20:50:03 +0100

    Hasher implementation, calculate_hash function

--- src/Hasher.cpp
+++ src/Hasher.cpp
@@ -28,5 +28,41 @@ std::string Hasher::calculate_hash(const std::filesystem::path &filepath)
         throw std::runtime_error("cannot open file: " + filepath.string());
     }
 
-    return "";
+    // ? reseting the context
+    if (EVP_DigestInit_ex(context, EVP_sha256(), nullptr) != 1)
+    {
+        throw std::runtime_error("OSSL Digest init failed");
+    }
+
+    // ? reading the file in 4KB chunks
+    const size_t buffer_size = 4096;
+    char buffer[buffer_size];
+
+    while (file.good())
+    {
+        file.read(buffer, buffer_size);
+
+        if (EVP_DigestUpdate(context, buffer, file.gcount()) != 1)
+        {
+            throw std::runtime_error("OSSL Digest update failed");
+        }
+    }
+
+    unsigned char hash_out[EVP_MAX_MD_SIZE];
+    unsigned int length_of_hash = 0;
+
+    if (EVP_DigestFinal_ex(context, hash_out, &length_of_hash) != 1)
+    {
+        throw std::runtime_error("OSSL Diegest Final failed");
+    }
+
+    // ? convert to hex string
+    std::stringstream ss;
+
+    for (unsigned int i = 0; i < length_of_hash; ++i)
+    {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash_out[i];
+    }
+
+    return ss.str();
 }
